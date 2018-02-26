716400c317f94af397dc9c60703f7cd42be2c29e
Anwar Montasir <anwarmontasir@gmail.com>
2018-02-26 11:25:37 -0800

    tests pass now. App and Header created

---
+++ src/components/app/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Header from './Header';
+// import Search from '../search/Search';
+// import MovieDetail from '../movies/MovieDetail';
+
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    
+    return (
+      <Router>
+        <div>
+          <Header/>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Redirect to="/"/>
+            </Switch>
+          </main>
+          <footer>&copy; 2018 this app.</footer>
+        </div>
+      </Router>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return <div>I am Home!</div>;
+  }
+}
